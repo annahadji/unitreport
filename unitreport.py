1b830d116ceffa8b33d54567cdf978815366f460
Anna Hadjitofi <annahadji@users.noreply.github.com>
2020-07-03 20:29:20 +0100

    Add plotting decorator and mypi init file.
    Co-authored-by: nuric <nuric@users.noreply.github.com>

---
+++ unitreport.py
@@ -0,0 +1,49 @@
+"""Unittest-based data quality and report generation tool."""
+import io
+import unittest
+import logging
+from typing import Dict, Callable, Optional, Tuple
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+FIGURES: Dict[str, Dict[str, str]] = dict()
+logger = logging.getLogger(__name__)
+
+
+def clear_figures():
+    """Clear global figure dictionary."""
+    logger.debug("Going to clear %i figures.", len(FIGURES))
+    FIGURES.clear()
+
+
+def plotting(
+    func: Callable[..., Optional[matplotlib.figure.Figure]],
+    figsize: Tuple[float, float] = (8, 6),
+):
+    """Decorator for tests that generate matplotlib figures.
+
+    Args:
+        func: 
+        figsize: [description]. Defaults to (8, 6).
+    """
+
+    def wrapper(*args, **kwargs):
+        # create clean figure for test case to utilise
+        plt.figure(figsize=figsize)
+        # call test case, if fails will raise error and rest of plot saving will not run
+        fig = func(*args, **kwargs) or plt
+
+        # save plot and description to buffer
+        buffer = io.StringIO()
+        fig.savefig(buffer, format="svg", bbox_inches="tight")
+        FIGURES[func.__name__] = {
+            "type": "plot",
+            "content": buffer.getvalue(),
+            "description": func.__doc__ or "",
+        }
+        logger.debug(
+            "Ran %s with buffer size %i.", func.__name__, len(buffer.getvalue())
+        )
+
+    return wrapper
