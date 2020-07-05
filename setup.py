0186aedd7c5f65d76ee6e468dcb25a7a556d6f0b
Anna Hadjitofi <annahadji@users.noreply.github.com>
2020-07-05 21:03:12 +0100

    Add setup.py and workflow publish to PyPI.
    Co-authored-by: nuric <nuric@users.noreply.github.com>

---
+++ setup.py
@@ -0,0 +1,29 @@
+"""Distribution script for unitreport."""
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="unitreport",
+    version="0.0.1",
+    author="annahadji",
+    author_email="annahadji@users.noreply.github.com",
+    description="A small unittest-based tool for generating single page html reports in Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="static unittest report generator Markdown plots tables",
+    url="https://github.com/annahadji/unitreport",
+    packages=["unitreport"],
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+    python_requires=">=3.6",
+    install_requires=["jinja2", "markdown", "matplotlib"],
+)
