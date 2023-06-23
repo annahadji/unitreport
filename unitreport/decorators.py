b969dbb8d67378d71b285de991eef2d3e0e7778f
Anna Hadjitofi <annahadji@users.noreply.github.com>
2023-06-23 21:40:20 +0200

    Fix using arguments with plotting decorator

--- unitreport/decorators.py
+++ unitreport/decorators.py
@@ -20,7 +20,6 @@ TestCaseType = TypeVar("TestCaseType", bound=unittest.TestCase)
 
 
 def plotting(
-    func: Callable[[TestCaseType], Optional[matplotlib.figure.Figure]],
     figsize: Tuple[float, float] = (8, 6),
     dpi: int = 100,
     save_to_disk: bool = False,
@@ -28,35 +27,43 @@ def plotting(
     """Decorator for tests that generate matplotlib figures.
 
     Args:
-        func: function passed to decorator.
         figsize: (plt) figure size. Defaults to (8, 6).
         dpi: (plt) figure dpi. Defaults to 100.
         save_to_disk: save figure separately to disk. Defaults to False.
     """
 
-    def wrapper(testcase: TestCaseType):
-        # create clean figure for test case to utilise
-        plt.figure(figsize=figsize, dpi=dpi)
-        # call test case, if fails will raise error and rest of plot saving will not run
-        fig = func(testcase) or plt
-        # save to in-memory buffer to avoid writing to file
-        buffer = io.StringIO()
-        fig.savefig(buffer, format="svg", bbox_inches="tight")
-        if save_to_disk:
-            fig.savefig(f"{func.__name__}.png", transparent=True, bbox_inches="tight")
-        # dont include empty plot in report
-        testcase.assertTrue(len(buffer.getvalue()), "Generated plot with empty output.")
-        # if above assertion fails, following code will not be run
-        FIGURES[func.__name__] = {
-            "type": "plot",
-            "content": buffer.getvalue(),
-            "description": func.__doc__ or "",
-        }
-        logger.debug(
-            "Ran %s with buffer size %i.", func.__name__, len(buffer.getvalue())
-        )
+    def plotting_decorator(
+        func: Callable[[TestCaseType], Optional[matplotlib.figure.Figure]]
+    ):
+        def wrapper(testcase: TestCaseType):
+            # create clean figure for test case to utilise
+            plt.figure(figsize=figsize, dpi=dpi)
+            # call test case, if fails will raise error and rest of plot saving will not run
+            fig = func(testcase) or plt
+            # save to in-memory buffer to avoid writing to file
+            buffer = io.StringIO()
+            fig.savefig(buffer, format="svg", bbox_inches="tight")
+            if save_to_disk:
+                fig.savefig(
+                    f"{func.__name__}.png", transparent=True, bbox_inches="tight"
+                )
+            # dont include empty plot in report
+            testcase.assertTrue(
+                len(buffer.getvalue()), "Generated plot with empty output."
+            )
+            # if above assertion fails, following code will not be run
+            FIGURES[func.__name__] = {
+                "type": "plot",
+                "content": buffer.getvalue(),
+                "description": func.__doc__ or "",
+            }
+            logger.debug(
+                "Ran %s with buffer size %i.", func.__name__, len(buffer.getvalue())
+            )
 
-    return wrapper
+        return wrapper
+
+    return plotting_decorator
 
 
 def tabling(func: Callable[[TestCaseType], str]):
